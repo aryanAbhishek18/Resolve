86c538efc86dfaeb6b59d8d587e246b67b574fd4
aryanAbhishek18 <aryan.abhishek1829@gmail.com>
2020-05-29 23:08:50 +0530

    Added production build

---
+++ react_resolve/build/precache-manifest.64501191b53d64855f1bf7330300304c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b0c144d99886f58b678",
+    "url": "/static/css/main.94e8dda0.chunk.css"
+  },
+  {
+    "revision": "6b0c144d99886f58b678",
+    "url": "/static/js/main.e49f694c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "488b98ef70225dee752b",
+    "url": "/static/js/2.b4104edf.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "46927bf505d4c5eb8a7321dd2f2fba8e",
+    "url": "/index.html"
+  }
+];
